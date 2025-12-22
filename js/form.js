162d445eb861c0460bfe43a3f087435546bb7b9c
qamastermanagement-pixel <qamastermanagement@gmail.com>
2025-12-22 09:26:39 +0700

    Update APPS_SCRIPT_URL in configuration

--- js/form.js
+++ js/form.js
@@ -119,7 +119,7 @@ const CHANNEL_MASTERS = {
 
 // Configuration object
 const CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxNF9XVOIyF-vcQcfzCR9XTW9ysb5GRu2e26nNW9207ftUm-YpCJwYHz3MRucv5DdTMKA/exec",
 }
 
 // Initialize form when page loads
