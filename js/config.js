de6009428a37283c97d9ee72ec86a89744697fce
qamastermanagement-pixel <qamastermanagement@gmail.com>
2025-12-22 09:52:51 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,8 @@
 // Google Apps Script Web App URL
 const CONFIG = {
   APPS_SCRIPT_URL:
-    "https://script.google.com/macros/s/AKfycbxNF9XVOIyF-vcQcfzCR9XTW9ysb5GRu2e26nNW9207ftUm-YpCJwYHz3MRucv5DdTMKA/exec",
+    "https://script.google.com/macros/s/AKfycbwua__3BChLPaxoi3oAwqslKiWZ7qD3phmA2qxBOy6bphS537xbtZHRJBmGDCykxzXqpQ/exec",
 }
+
+// Make CONFIG available globally
+window.CONFIG = CONFIG
