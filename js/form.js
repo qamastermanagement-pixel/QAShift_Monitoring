22a12397ac89dbe0b76dab39183e3d4cc9337b5e
qamastermanagement-pixel <qamastermanagement@gmail.com>
2025-12-23 14:32:37 +0700

    Update form.js

--- js/form.js
+++ js/form.js
@@ -454,7 +454,7 @@ async function submitData() {
   console.log("[v0] Submitting data...")
   const appsScriptUrl = window.CONFIG
     ? window.CONFIG.APPS_SCRIPT_URL
-    : "https://script.google.com/macros/library/d/1OfmVtkDn9-cWhWeAYC5IqK6G9qhdLCpHW-Pzpqraty7BY_AAMDWfO0xq/6"
+    : "https://script.google.com/macros/s/AKfycbytpHuYFDR_G-sugVMYFVpEbw1uQObHt68HiiRsuo01YybVLh_otjhjW971CO9QrH5gtA/exec"
   console.log("[v0] Apps Script URL:", appsScriptUrl)
 
   // Collect master check results
