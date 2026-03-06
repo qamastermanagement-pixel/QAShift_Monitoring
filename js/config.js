30ee499aa75a42eed2ceb65e37e098915e3ca7d1
qamastermanagement-pixel <qamastermanagement@gmail.com>
2026-03-06 10:50:18 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ window.CONFIG = {
     "https://script.google.com/macros/s/AKfycbytpHuYFDR_G-sugVMYFVpEbw1uQObHt68HiiRsuo01YybVLh_otjhjW971CO9QrH5gtA/exec",
 
   VERIFY_APPS_SCRIPT_URL:
-    "https://script.google.com/macros/s/AKfycbzofsTElmr6GWd5H4BJHB0So7JSspX9qYaGfKv1FwMsASCcE5WYTRqcAwenpkXLsw_q/exec",
+    "https://script.google.com/macros/s/AKfycbx-OZHh2ZuI51LVlU8wxRvc4vsiVPK6OVfvYZl4e6OQ5tehc1FPZQzTVskIq5WIaTVM/exec",
 
   MASTER_DATA_URL:
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vTrVEMf_DG702fbz5Gy12__YvNYc1lNXTW-gFcZbV5J0NSndYYvjQb_HmjsEWImsZBLAEZqlTs9eLDh/pub?gid=0&single=true&output=csv",
