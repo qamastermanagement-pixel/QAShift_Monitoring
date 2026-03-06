ed12c8b6c132dc85ea846519ae1f29e9aaea46d6
qamastermanagement-pixel <qamastermanagement@gmail.com>
2026-03-06 09:13:17 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,4 +13,9 @@ window.CONFIG = {
   // MASTER DATA (FORM CH0)
   MASTER_DATA_URL_CH0:
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vTrVEMf_DG702fbz5Gy12__YvNYc1lNXTW-gFcZbV5J0NSndYYvjQb_HmjsEWImsZBLAEZqlTs9eLDh/pub?gid=1956008952&single=true&output=csv",
+ 
+  // VERIFY CERTIFICATE OF CALIBRATION
+  VERIFY_APPS_SCRIPT_URL:
+    "https://script.google.com/macros/s/AKfycbzofsTElmr6GWd5H4BJHB0So7JSspX9qYaGfKv1FwMsASCcE5WYTRqcAwenpkXLsw_q/exec",
+
 };
