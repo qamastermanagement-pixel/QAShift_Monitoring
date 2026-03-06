3043e7fadf5b7e8030e5896d6b30e99b41fd7690
qamastermanagement-pixel <qamastermanagement@gmail.com>
2026-03-06 09:16:08 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,21 +1,15 @@
-// js/config.js
-// Pastikan Google Sheets sudah "Publish to web" sebagai CSV
-
 window.CONFIG = {
-  // Google Apps Script Web App URL (SUBMIT & DASHBOARD)
+
   APPS_SCRIPT_URL:
     "https://script.google.com/macros/s/AKfycbytpHuYFDR_G-sugVMYFVpEbw1uQObHt68HiiRsuo01YybVLh_otjhjW971CO9QrH5gtA/exec",
 
-  // MASTER DATA (FORM UTAMA)
+  VERIFY_APPS_SCRIPT_URL:
+    "https://script.google.com/macros/s/AKfycbzofsTElmr6GWd5H4BJHB0So7JSspX9qYaGfKv1FwMsASCcE5WYTRqcAwenpkXLsw_q/exec",
+
   MASTER_DATA_URL:
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vTrVEMf_DG702fbz5Gy12__YvNYc1lNXTW-gFcZbV5J0NSndYYvjQb_HmjsEWImsZBLAEZqlTs9eLDh/pub?gid=0&single=true&output=csv",
 
-  // MASTER DATA (FORM CH0)
   MASTER_DATA_URL_CH0:
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vTrVEMf_DG702fbz5Gy12__YvNYc1lNXTW-gFcZbV5J0NSndYYvjQb_HmjsEWImsZBLAEZqlTs9eLDh/pub?gid=1956008952&single=true&output=csv",
- 
-  // VERIFY CERTIFICATE OF CALIBRATION
-  VERIFY_APPS_SCRIPT_URL:
-    "https://script.google.com/macros/s/AKfycbzofsTElmr6GWd5H4BJHB0So7JSspX9qYaGfKv1FwMsASCcE5WYTRqcAwenpkXLsw_q/exec",
 
 };
